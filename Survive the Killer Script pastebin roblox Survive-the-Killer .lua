7ad1d7107161cce06dba275cb944c6a74a1b6de8
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:52:32 +0100

    commit zero

---
+++ Survive the Killer Script pastebin roblox Survive-the-Killer .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://fluxteam.xyz/scripts/STK.lua", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script with collecting coins and esp maniac or survivors
+						
+]]
